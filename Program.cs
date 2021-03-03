72749463c8e5f5db46250a13386767ca1062ac01
60mts <62522168+60mts@users.noreply.github.com>
2021-03-03 23:44:13 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictinory_odev
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Tohat");
+
+
+            myList<string> sehirler2 = new myList<string>();
+            sehirler2.Add("Diyarbakır");
+            }
+            class myList<T>
+            {
+            T[] _array;
+            T[] _tempArray;
+                
+            public void Add(T item)
+                {
+                _tempArray = _array;
+                _array  = new  T[_array.Length + 1];
+                }
+
+            }
+                
+            }
+        }
+    
